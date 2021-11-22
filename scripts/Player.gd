afe61b31cd708c4be49ad9d5f66cd1024e499c5c
mlange-42 <martin_lange_@gmx.net>
2021-11-22 23:37:52 +0100

    implement basic movement around corners / gravity switching

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,7 +8,10 @@ export (float, 0, 1.0) var friction = 0.25
 export (float, 0, 1.0) var acceleration = 0.35
 export (float, 0, 1.0) var air_factor = 0.2
 
-onready var sprite = $Sprite
+onready var sprite: Sprite = $Sprite
+onready var ray_left: RayCast2D = $RayLeft
+onready var ray_right: RayCast2D = $RayRight
+onready var ray_down: RayCast2D = $RayDown
 
 var velocity = Vector2.ZERO
 
@@ -25,6 +28,9 @@ func get_input():
 		velocity.x = lerp(velocity.x, dir * speed, fac * acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, fac * friction)
+	
+	if not Input.is_action_pressed("dont_stick"):
+		check_rotation()
 
 func _process(delta):
 	if self.is_on_floor():
@@ -34,8 +40,55 @@ func _process(delta):
 
 func _physics_process(delta):
 	get_input()
+	
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity = move_and_slide(velocity.rotated(self.rotation), Vector2.UP.rotated(self.rotation))
+	velocity = velocity.rotated(-self.rotation)
+	
+	if abs(velocity.x) < 1:
+		velocity.x = 0
+	
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = jump_speed
+	
+	update()
+
+func check_rotation():
+	if not self.is_on_floor():
+		return
+	
+	# Concave corners
+	
+	var ray: RayCast2D = null
+	var dir: int = 0
+	if velocity.x < 0:
+		ray = ray_left
+		dir = 1
+	elif velocity.x > 0:
+		ray = ray_right
+		dir = -1
+	
+	if ray != null:
+		ray.force_raycast_update()
+		if ray.is_colliding():
+			self.rotate(PI * 0.5 * dir)
+			return
+	
+	# Convex corners
+	
+	ray_down.force_raycast_update()
+	if ray_down.is_colliding():
+		return
+	
+	var angle = PI * 0.5 * sign(velocity.x)
+	
+	var center = self.position + Vector2(0, 35).rotated(self.rotation)
+	self.position = center + (self.position - center).rotated(angle)
+	self.rotate(angle)
+	
+
+func _draw():
+	draw_line(Vector2.ZERO, Vector2.UP * 50, Color.red)
+	draw_line(Vector2.ZERO, velocity * 0.2, Color.green)
+	
