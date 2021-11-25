660a8d696e1e782c35a1d905c085a11268ba12d9
mlange-42 <martin_lange_@gmx.net>
2021-11-26 00:52:17 +0100

    add level entrance to go backwards

--- scripts/objects/Entrance.gd
+++ scripts/objects/Entrance.gd
@@ -1,9 +1,9 @@
 extends Interactable
 
-class_name Exit
+class_name Entrance
 
 func interact(_player):
-	var level = Levels.current_level + 1
+	var level = Levels.current_level - 1
 	if not Levels.load_level(level):
 		if get_tree().change_scene("scenes/MainMenu.tscn") == OK:
 			Levels.current_level = 0
