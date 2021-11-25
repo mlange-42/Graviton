a5bd11f07bd1ef0afbc5a065ed56ba404a18537b
mlange-42 <martin_lange_@gmx.net>
2021-11-25 23:21:31 +0100

    add main menu and simple level structure

---
+++ scripts/Levels.gd
@@ -0,0 +1,26 @@
+extends Node
+
+var current_level: int = 0
+
+var levels: Array = [
+	"Intro",
+	"L1",
+	"L2",
+	"L3",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4",
+	"L4"
+]
--- scripts/objects/Exit.gd
+++ scripts/objects/Exit.gd
@@ -3,5 +3,11 @@ extends Interactable
 class_name Exit
 
 func interact(_player):
-	print("EXITING")
-	
+	var level = Levels.current_level + 1
+	if Levels.levels.size() > level:
+		var scene = Levels.levels[level] + ".tscn"
+		if get_tree().change_scene("scenes/levels/"+scene) == OK:
+			Levels.current_level = level
+	else:
+		if get_tree().change_scene("scenes/MainMenu.tscn") == OK:
+			Levels.current_level = 0
