86bda94165cb1fc52852b1baa7a30a92ae0e9b1f
mlange-42 <martin_lange_@gmx.net>
2021-11-26 17:31:21 +0100

    save and load player progress

--- scripts/Levels.gd
+++ scripts/Levels.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const store_file: String = "user://graviton.sav"
+
 var player_level: int = 0
 var current_level: int = 0
 var go_back: bool = false
@@ -13,6 +15,9 @@ var levels: Array = [
 	"Level_006"
 ]
 
+func _ready():
+	load_user()
+
 func load_level(index) -> bool:
 	go_back = index == current_level - 1
 	if index >= 0 and index < levels.size():
@@ -21,7 +26,23 @@ func load_level(index) -> bool:
 			current_level = index
 			if player_level < current_level:
 				player_level = current_level
+				save_user()
 				
 			return true
 	
 	return false
+
+func save_user():
+	var file = File.new()
+	file.open(store_file, File.WRITE)
+	file.store_var(player_level)
+	file.close()
+
+func load_user():
+	var file = File.new()
+	if file.file_exists(store_file):
+		file.open(store_file, File.READ)
+		player_level = file.get_var()
+		file.close()
+	else:
+		player_level = 0
